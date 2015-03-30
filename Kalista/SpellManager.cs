42bf1e0698acb76ecf5b03710d6d4e4c78d8e6f6
kolo1477 <kolo1477@wp.pl>
2015-03-31 01:02:11 +0200

    Create SpellManager.cs

---
+++ Kalista/SpellManager.cs
@@ -0,0 +1,40 @@
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Kalista
+{
+    public static class SpellManager
+    {
+        public static Spell Q { get; private set; }
+        public static Spell W { get; private set; }
+        public static Spell E { get; private set; }
+        public static Spell R { get; private set; }
+
+        static SpellManager()
+        {
+            // Initialize spells
+            Q = new Spell(SpellSlot.Q, 1150);
+            W = new Spell(SpellSlot.W, 5200);
+            E = new Spell(SpellSlot.E, 1000);
+            R = new Spell(SpellSlot.R, 1400);
+
+            // Finetune spells
+            Q.SetSkillshot(0.25f, 40, 1700, true, SkillshotType.SkillshotLine);
+        }
+
+        public static Spell GetSpellFromSlot(SpellSlot slot)
+        {
+            return slot == SpellSlot.Q ? Q : slot == SpellSlot.W ? W : slot == SpellSlot.E ? E : slot == SpellSlot.R ? R : null;
+        }
+
+        public static bool IsEnabled(this Spell spell, string mode)
+        {
+            return Config.BoolLinks[string.Concat(mode, "Use", spell.Slot.ToString())].Value;
+        }
+
+        public static bool IsEnabledAndReady(this Spell spell, string mode)
+        {
+            return spell.IsEnabled(mode) && spell.IsReady();
+        }
+    }
+}
